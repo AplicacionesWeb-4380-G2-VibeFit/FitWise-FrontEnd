9887637ad8c7a9e0db9b0296360c69a134875b81
Miguel26112001 <u202220294@upc.edu.pe>
2025-05-13 01:53:43 -0500

    chore: env and api implementation

---
+++ src/shared/services/http.instance.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const baseApiUrl = import.meta.env.VITE_API_BASE_URL
+
+const httpInstance = axios.create({
+    baseURL: baseApiUrl,
+    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+});
+
+export default httpInstance;
