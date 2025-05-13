c954c98fbddb84555f77ac0b2fea985f342adae2
Miguel26112001 <u202220294@upc.edu.pe>
2025-05-13 01:13:58 -0500

    chore: added first home view

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <script >
+import HeaderContent from "@/public/components/header-content.component.vue";
+import FooterContent from "@/public/components/footer-content.component.vue";
+
 export default {
   name: "App",
+  components: { FooterContent, HeaderContent },
+  data() {
+    return {
+      items: [
+        {label: 'option.home',       to: '/home'},
+        {label: 'option.about',      to: '/about'}
+      ]
+    }
+  }
 }
 </script>
 
 <template>
+  <pv-toast/>
+  <pv-confirm-dialog/>
 
+  <header-content :items="items"/>
+
+  <main>
+    <router-view/>
+  </main>
+
+  <footer-content/>
 </template>
 
 <style scoped>
-
+main {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
 </style>
