c0c9d00f5d6fcedc6e75f969e30e85686eb45fe3
Miguel26112001 <u202220294@upc.edu.pe>
2025-05-14 01:39:36 -0500

    feat: added meal.service

---
+++ src/publishing/services/meal.service.js
@@ -0,0 +1,33 @@
+import httpInstance from "@/shared/services/http.instance.js";
+
+export class MealService {
+    resourceEndpoint = import.meta.env.VITE_MEALS_ENDPOINT_PATH;
+
+    getAll() {
+        return httpInstance.get(this.resourceEndpoint);
+    }
+
+    getById(id) {
+        return httpInstance.get(`${this.resourceEndpoint}/${id}`);
+    }
+
+    getByHealthPlanId(healthPlanId) {
+        return httpInstance.get(this.resourceEndpoint, {
+            params: {
+                healthPlanId: healthPlanId
+            }
+        });
+    }
+
+    create(resource) {
+        return httpInstance.post(this.resourceEndpoint, resource);
+    }
+
+    update(id, resource) {
+        return httpInstance.put(`${this.resourceEndpoint}/${id}`, resource);
+    }
+
+    delete(id) {
+        return httpInstance.delete(`${this.resourceEndpoint}/${id}`);
+    }
+}
