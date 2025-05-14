ec61bc3fe7c763d35b6c79f67b7ea1e581d5764b
Miguel26112001 <u202220294@upc.edu.pe>
2025-05-14 04:18:56 -0500

    feat: add healthPlan Detail

--- src/publishing/pages/health-plan-details.component.vue
+++ src/publishing/pages/health-plan-details.component.vue
@@ -1,11 +1,37 @@
 <script>
+import HealthPlanInfo from "@/publishing/components/health-plan-info.component.vue";
+import {HealthPlan} from "@/publishing/model/health-plan.entity.js";
+import {HealthPlanService} from "@/publishing/services/health-plan.service.js";
+
 export default {
-  name: "health-plan-details"
+  name: "health-plan-details",
+  components: {HealthPlanInfo},
+  data() {
+    return {
+      healthPlan: new HealthPlan({}),
+      healthPlanService: null,
+      errors: [],
+    };
+  },
+  methods: {
+    getHealthPlanById(id) {
+      this.healthPlanService.getById(id).then(response => {
+        console.log(response);
+        this.healthPlan = new HealthPlan(response.data);
+        console.log(this.healthPlan);
+      }).catch(error => { this.errors.push(error); this.healthPlan = null; console.log(error); });
+    }
+  },
+  created() {
+    this.healthPlanService = new HealthPlanService();
+    const id = Number(this.$route.params.id);
+    this.getHealthPlanById(id);
+  }
 }
 </script>
 
 <template>
-  <h1>Miguelito</h1>
+  <health-plan-info :health-plan="healthPlan"/>
 </template>
 
 <style scoped>
